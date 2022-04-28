17dccada6b35e7421211ab480ee8406c4ad74105
tdavis0828 <davis.t828@gmail.com>
2022-04-28 14:16:22 -0700

    File restructuring

--- ReactProjects/number-guesser-game/src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-const Button = ({ type, className, onClick, children, id = "" }) => {
-  return (
-    <button type={type} className={className} onClick={onClick} id={id}>
-      {children}
-    </button>
-  );
-};
-
-export default Button;
